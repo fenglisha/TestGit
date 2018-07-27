4d8bade0f0dd159d0b6cc06d0d95ce7dff6e61c4
fenglisha <3113013893@qq.com>
2018-07-27 14:50:29 +0800

    jjj

--- MianSi/src/com/first/test/DateTest.java
+++ MianSi/src/com/first/test/DateTest.java
@@ -3,13 +3,15 @@ package com.first.test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-//测试git,修改下
+
+//娴嬭瘯涓�啥有不想要
+//jhjh
 public class DateTest {
 	public static void main(String[] args) {
 
-		// 获取当前时间
+		// 鑾峰彇褰撳墠鏃堕棿
 		Date date = new Date();
-		// 字符串转时间 parse
+		// 瀛楃涓茶浆鏃堕棿 parse
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		try {
 			dateFormat.parse("2017-06-16 11:03:52");
@@ -18,13 +20,13 @@ public class DateTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// 获取当前时间的前五天,getTIme是ms,得到long类型再换算;
+		// 鑾峰彇褰撳墠鏃堕棿鐨勫墠浜斿ぉ,getTIme鏄痬s,寰楀埌long绫诲瀷鍐嶆崲绠�;
 		long datelong = date.getTime() - 5 * 24 * 60 * 60 * 1000;
 		Date date5 = new Date(datelong);
 		System.out.println(dateFormat.format(date5));
-		// 分开得到时间的年月日,只能用字符串截取；
+		// 鍒嗗紑寰楀埌鏃堕棿鐨勫勾鏈堟棩,鍙兘鐢ㄥ瓧绗︿覆鎴彇锛�
 		String dateString = dateFormat.format(date5);
-		// "yyyy-MM-dd HH:mm:ss";substring:取前不取后；
+		// "yyyy-MM-dd HH:mm:ss";substring:鍙栧墠涓嶅彇鍚庯紱
 		String year = dateString.substring(0, 4);
 		System.out.println(year);
 
